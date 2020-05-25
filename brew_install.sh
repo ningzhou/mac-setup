2e8e27795ad672990a2e2aced1b1cecd70f8ce5c
Zhou <ningzho@3c22fb0e9f8d.ant.amazon.com>
2020-05-24 23:46:16 -0700

    Add startup script and script to install core formulaes and apps using homebrew

---
+++ brew_install.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install Homebrew if we don't have it
+if test ! $(which brew); then
+  echo "------------------------------"
+  echo "Installing homebrew..."
+  echo "------------------------------"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade 
+
+# Install GNU core utilities (those that come with OS X are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH` if we want to use the gnu utils as default options.
+echo "------------------------------"
+echo "Installing gnu coreuitls, moreutils, findutils, gnu-sed..."
+echo "------------------------------"
+# Insall coreuitls
+brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed
+
+
+echo "------------------------------"
+echo "Installing wget"
+echo "------------------------------"
+# Install `wget` with IRI support.
+# Options has been removed for homebrew core formulaes
+# brew install wget  --with-iri 
+brew install wget 
+
+# Install zsh and set it as the default  
+echo "------------------------------"
+echo "Installing Zsh and change it to default shell"
+echo "------------------------------"
+brew install zsh
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/zsh" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+
+echo "------------------------------"
+echo "Installing must-have apps via brew cask"
+echo "------------------------------"
+
+# Core casks
+brew cask install --appdir="/Applications" alfred
+brew cask install --appdir="/Applications" iterm2
+brew cask install --appdir="/Applications" emacs
+brew cask install --appdir="~/Applications" xquartz
+brew cask install --appdir="~/Applications" karabiner-elements
+
+# Remove outdated versions from the cellar.
+brew cleanup
---
+++ setup.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+function run_setup() {
+    # Ask for the administrator password upfront
+    sudo -v
+
+    # Keep-alive: update existing `sudo` time stamp until the script has finished
+    while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+    # Run sections based on command line arguments
+    for ARG in "$@"
+    do
+        if [ $ARG == "brew" ] || [ $ARG == "all" ]; then
+            # Run the brew_install.sh Script
+            # For a full listing of installed formulae and apps, refer to
+            # the commented brew_install.sh source file directly 
+            echo ""
+            echo "------------------------------"
+            echo "Installing Homebrew along with some common formulae and apps."
+            echo "This might take a while to complete, as some formulae need to be installed from source."
+            echo "------------------------------"
+            echo ""
+            bash brew_install.sh
+        fi
+
+        if [ $ARG == "omz" ] || [ $ARG == "all" ]; then
+	    echo ""
+	    echo "------------------------------"
+	    echo "Installing oh-my-zsh"
+	    echo "------------------------------"
+	    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	    echo ""
+	fi
+
+        if [ $ARG == "config" ] || [ $ARG == "all" ]; then
+            echo ""
+            echo "------------------------------"
+            echo "Syncing the configuration files for a number of apps into home directory."
+            echo "------------------------------"
+            echo ""
+        fi
+    done
+
+    echo "------------------------------"
+    echo "Completed setting up with your options, restart your computer to ensure all updates take effect"
+    echo "------------------------------"
+}
+
+read -p "This script may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    run_setup $@
+fi;
+
+unset run_setup;
+
